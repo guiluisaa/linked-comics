027c4e58ef3db6ab7c61b04fe067286f7a0d9241
Guilherme Araujo <guiluis.aa@gmail.com>
2018-12-14 13:13:08 -0200

    Comic show page

---
+++ app/comics/show/controller.js
@@ -0,0 +1,11 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+
+  actions: {
+    addToCart() {
+      console.log('Add to Cart');
+    }
+  }
+
+});
--- app/comics/show/route.js
+++ app/comics/show/route.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
+import $ from 'jquery';
 
 export default Route.extend({
+
+  model(params) {
+
+    return $.ajax(`http://gateway.marvel.com/v1/public/comics/${params.comic_id}?ts=1&apikey=ac75869b0cff28a01af6bb94480dea66&hash=5b838e5aa74f1ef4a48741ab69dbfc7f&dateRange=2013-01-01,2013-01-02`)
+      .then(response => {
+        return response.data.results[0];
+      });
+  }
+
 });
