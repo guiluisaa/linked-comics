7019f22d70d0126d838b8369743709cca14d5efb
Guilherme Araujo <guiluis.aa@gmail.com>
2018-12-14 03:00:10 -0200

    Add SASS and bootstrap

--- app/comics/index/route.js
+++ app/comics/index/route.js
@@ -5,7 +5,7 @@ export default Route.extend({
 
   model() {
 
-    return $.ajax('http://gateway.marvel.com/v1/public/comics?ts=1&apikey=ac75869b0cff28a01af6bb94480dea66&hash=5b838e5aa74f1ef4a48741ab69dbfc7f')
+    return $.ajax('http://gateway.marvel.com/v1/public/comics?ts=1&apikey=ac75869b0cff28a01af6bb94480dea66&hash=5b838e5aa74f1ef4a48741ab69dbfc7f&dateRange=2013-01-01,2013-01-02')
       .then(response => {
         return response.data.results;
       });
